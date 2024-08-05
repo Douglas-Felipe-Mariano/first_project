d843beede4508688e23d3933efa07a1356371cb0
Douglas-Felipe-Mariano <159494725+Douglas-Felipe-Mariano@users.noreply.github.com>
2024-08-05 20:38:27 -0300

    Aula 05/08/24 (Organização de rotas)

--- src/app/usuarios/login/page.jsx
+++ src/app/usuarios/login/page.jsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css"
 
-export default function Home() {
+export default function login() {
   return (
     <div className='container'>
       <div className={styles.container_title}>
-        <h1 className={styles.txt_titulo}>Next.js</h1>
+        <h1 className={styles.txt_titulo}>login</h1>
       </div>
       
     </div>
