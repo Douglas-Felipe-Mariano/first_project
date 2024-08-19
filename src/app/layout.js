4e205af0c3c125c63e68f654278f6a6fb3241eda
Douglas-Felipe-Mariano <159494725+Douglas-Felipe-Mariano@users.noreply.github.com>
2024-08-19 20:15:02 -0300

    header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 // import { Inter } from "next/font/google";
 import "./globals.css";
 import Rodape from "@/componentes/rodape";
+import Header from "@/componentes/cabecalho";
 
 // const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
       {/* <body className={inter.className}>{children}</body> */}
       <body>
         <div className="layout">
+          <Header/>
           {children}
           <Rodape/>
         </div>
