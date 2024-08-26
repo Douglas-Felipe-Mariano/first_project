c8f14f281369c8cc700569be0c05656d26efea70
Douglas-Felipe-Mariano <159494725+Douglas-Felipe-Mariano@users.noreply.github.com>
2024-08-26 20:40:03 -0300

    Deixando igual o do weverton

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,31 +1,25 @@
+import { MdLunchDining, MdLocalBar, MdDining, MdIcecream, MdFastfood  } from "react-icons/md";
+
 import Image from "next/image";
 import styles from "./page.module.css"
+import ProdutosHome from "@/componentes/produtos/produtosHome";
+import Slider from "@/componentes/slider";
 
 export default function Home() {
   return (
     <div className='containerGlobal'>
       <div className={styles.container_title}>
-        <h1 className={styles.txt_titulo}>Next.js</h1>
-        <h2>Exemplo h2</h2>
-        <h3>Exemplo h3</h3>
-        <p>Exemplo Paragrafo</p>
-        
-        <div style={styles.containterImagem}>
-          <Image
-            src={'/palmeiras.jpg'}
-            width={500}
-            height={500}
-            alt="Palmeiraaaaas"
-            className={styles.imagemHome}
-          />
-          <Image
-            src={'/palmeiras.jpg'}
-            width={1000}
-            height={1000}
-            alt="Palmeiraaaaas"
-            className={styles.imagemHome}
-          />
+        <Slider/>
+
+        <div className={styles.tipos}>
+          <MdLunchDining className={styles.tpicon}/>
+          <MdLocalBar className={styles.tpicon}/>
+          <MdDining className={styles.tpicon}/>
+          <MdIcecream className={styles.tpicon}/>
+          <MdFastfood className={styles.tpicon}/>
         </div>
+
+        <ProdutosHome/>
 
       </div>
     </div>
